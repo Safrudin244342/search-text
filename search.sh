9591eafe332acac35834c6a9144df13b1c5f6037
muhamad safrudin abdul azis <safrudin2443420@gmail.com>
2020-09-26 10:53:24 +0700

    prototipe

--- search.sh
+++ search.sh
@@ -1,23 +1,43 @@
 #!/bin/bash
-# Bash Menu Script Example
 
-PS3='Please enter your choice: '
-options=("Option 1" "Option 2" "Option 3" "Quit")
-select opt in "${options[@]}"
-do
-    case $opt in
-        "Option 1")
-            echo "you chose choice 1"
-            ;;
-        "Option 2")
-            echo "you chose choice 2"
-            ;;
-        "Option 3")
-            echo "you chose choice $REPLY which is $opt"
-            ;;
-        "Quit")
-            break
-            ;;
-        *) echo "invalid option $REPLY";;
-    esac
-done
+key=$1
+filename=$2
+lines=()
+
+function getString {
+  filename=$2
+  key=$1
+  while read -r line; do
+    lines[${#lines[@]}]="====================================="
+    lines[${#lines[@]}]="Search for key '$key' from '$filename'"
+    lines[${#lines[@]}]=$line
+  done < <(grep $filename -e "$key")
+}
+
+function showLines {
+  for ((i=0; i < ${#lines[@]}; i++)); do
+    echo ${lines[$i]}
+  done
+}
+
+function setShowLines {
+  show=10
+  len=${#lines[@]}
+  let "loop = len - show"
+  for (($show; $show < $loop; show++)); do
+    while read -r line; do
+      echo $line
+    done < <(showLines | head -n$show)
+    read 
+    clear
+    let show=show+1
+  done
+}
+
+function start {
+  clear
+  getString "$key" "$filename"
+  setShowLines
+}
+
+start
